ff5dec4d9712cb26b3e2f11a13fc5f87dbc14ca3
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-02-02 14:27:29 -0600

    fixed font-awesome and added comments to JS

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+/* 
+==========================================================================
 // Get footer date
+========================================================================== 
+*/
 const getYear = new Date().getFullYear();
 const fullYear = `${getYear}`;
 const getDate = document.getElementById("getDate");
